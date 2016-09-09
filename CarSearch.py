f5b762be978b6343c1edf4b56d0b2e823972869f
Dragos Rotaru <info@dragosrotaru.com>
2016-09-08 20:12:53 -0400

    finished, included sample csv file

--- CarSearch.py
+++ CarSearch.py
@@ -10,29 +10,29 @@ HEADERS = {
     'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36"
     }
 
-#TODO handle paging automatically
-#TODO user input of urls
-
+#put your urls here:
+#Kijiji: make sure url includes "page-1" in query string
+#AutoTRADER: make sure "rcp=" is included in the query string and the value is larger then result number
 AUTOTRADER_URL = "http://wwwa.autotrader.ca/cars/on/oakville/?prx=100&prv=Ontario&loc=L6H3V2&trans=Automatic&body=Sedan&sts=New-Used&pRng=4000%2c9000&oRng=10000%2c80000&yRng=2010%2c&hprc=True&wcp=True&inMarket=advancedSearch&rcs=0&rcp=10000"
-url1= "http://www.kijiji.ca/b-cars-trucks/oakville-halton-region/acura__audi__bmw__cadillac__ford__infiniti__lexus__mazda__mercedes+benz__mitsubishi__subaru__volkswagen__volvo-2010__/page-"
-url2 = "/c174l1700277a54a68r149.0?ad=offering&price=4000__9000&kilometers=10000__80000&transmission=2&no-of-seats=4__5"
+KIJIJI_URL= "http://www.kijiji.ca/b-cars-trucks/oakville-halton-region/acura__audi__bmw__cadillac__ford__infiniti__lexus__mazda__mercedes+benz__mitsubishi__subaru__volkswagen__volvo-2010__/page-1/c174l1700277a54a68r149.0?ad=offering&price=4000__9000&kilometers=10000__80000&transmission=2&no-of-seats=4__5"
 
+#page numbers for Kijiji Results
 lastPage = 2
 firstPage = 1
 
 columns = ["source", "date posted", "title", "description", "url", "transmission", "location", "year", "price", "mileage"]
-df = pd.DataFrame(columns=columns)
 
 
-def retrieveKijiji():
+def retrieveKijiji(df):
     print("retrieving Kijiji...")
+    count = 1
     for num in range(firstPage,lastPage + 1):
-        print("processing page " + str(num) + " of " + str(lastPage))
-        url = url1 + str(num) + url2
+        url = KIJIJI_URL.replace("page-1", "page-" + str(num))
         request = r.get(url, headers=HEADERS)
         soup = bs(request.text, 'html.parser')
         for html in soup.find_all("div", "search-item"):
-
+            print( "retrieving result ", count, end="\r")
+            count = count + 1
             tag = html.find("a", "title")
             title = tag.text
             year = tag.contents[0].split()[0]
@@ -78,14 +78,18 @@ def retrieveKijiji():
             tag = html.find("div", "location")
             location = tag.contents[0]
 
-def retrieveAutoTRADER():
-    print("retrieving autoTRADER...")
+            df2 = pd.DataFrame([["autoTRADER", datePosted, str(title).lower().strip(), str(description).lower().strip(), url, str(transmission).lower().strip(), str(location).lower().strip(), str(price).lower().strip(), str(year).lower().strip(), str(mileage).replace('km', '').lower().strip()]], columns=columns)
+            df = df.append(df2)
+
+    return df
+
+def retrieveAutoTRADER(df):
+    print("\nretrieving autoTRADER...")
     request = r.get(AUTOTRADER_URL, headers=HEADERS)
-    print("retrieved html")
     soup = bs(request.text, 'html.parser')
     count = 1
     for html in soup.find_all("div", "resultItem"):
-        print( "retrieving result ", count)
+        print( "retrieving result ", count, end="\r")
         count = count + 1
         tag = html.find("span", "resultTitle")
         title = tag.text
@@ -110,14 +114,15 @@ def retrieveAutoTRADER():
 
         df2 = pd.DataFrame([["autoTRADER", None, title.lower().strip(), description.lower().strip(), url, None, location.lower().strip(), price.lower().strip(), year.lower().strip(), mileage.replace('km', '').lower().strip()]], columns=columns)
         df = df.append(df2)
-
-
+    return df
 
 def main():
-    retrieveKijiji()
-    retrieveAutoTRADER()
-    print("done!")
-
+    df = pd.DataFrame(columns=columns)
+    df = df.append(retrieveKijiji(df))
+    df = df.append(retrieveAutoTRADER(df))
+    numberResults = len(df.index)
+    print("\ndone! " + str(numberResults) + " results found")
+    df = df.drop_duplicates()
     df.to_csv(datetime.datetime.today().strftime("%Y-%m-%d") + "-CarSearchOutput.csv", index=False)
 
 if __name__ == '__main__':
