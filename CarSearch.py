29b1fb788b9157af6d561460f5efa5a8c32c38e5
Dragos Rotaru <info@dragosrotaru.com>
2016-09-13 00:32:21 -0400

    some changes

--- CarSearch.py
+++ CarSearch.py
@@ -13,14 +13,14 @@ HEADERS = {
 #put your urls here:
 #Kijiji: make sure url includes "page-1" in query string
 #AutoTRADER: make sure "rcp=" is included in the query string and the value is larger then result number
-AUTOTRADER_URL = "http://wwwa.autotrader.ca/cars/on/oakville/?prx=100&prv=Ontario&loc=L6H3V2&trans=Automatic&body=Sedan&sts=New-Used&pRng=4000%2c9000&oRng=10000%2c80000&yRng=2010%2c&hprc=True&wcp=True&inMarket=advancedSearch&rcs=0&rcp=10000"
-KIJIJI_URL= "http://www.kijiji.ca/b-cars-trucks/oakville-halton-region/acura__audi__bmw__cadillac__ford__infiniti__lexus__mazda__mercedes+benz__mitsubishi__subaru__volkswagen__volvo-2010__/page-1/c174l1700277a54a68r149.0?ad=offering&price=4000__9000&kilometers=10000__80000&transmission=2&no-of-seats=4__5"
+AUTOTRADER_URL = "http://wwwa.autotrader.ca/cars/on/oakville/?prx=25&prv=Ontario&loc=L6H3V2&trans=Automatic&sts=New-Used&pRng=4000%2c8000&oRng=20000%2c100000&yRng=2010%2c2014&hprc=True&wcp=True&inMarket=advancedSearch&rcs=0&rcp=100"
+KIJIJI_URL= "http://www.kijiji.ca/b-cars-trucks/oakville-halton-region/2010__2015/page-1/c174l1700277a68r25.0?ad=offering&price=4000__8000&kilometers=20000__100000&transmission=2"
 
 #page numbers for Kijiji Results
 lastPage = 2
 firstPage = 1
 
-columns = ["source", "date posted", "title", "description", "url", "transmission", "location", "year", "price", "mileage"]
+columns = ["source", "date posted", "title", "description", "url", "transmission", "location", "price", "year", "mileage"]
 
 
 def retrieveKijiji(df):
@@ -78,9 +78,8 @@ def retrieveKijiji(df):
             tag = html.find("div", "location")
             location = tag.contents[0]
 
-            df2 = pd.DataFrame([["autoTRADER", datePosted, str(title).lower().strip(), str(description).lower().strip(), url, str(transmission).lower().strip(), str(location).lower().strip(), str(price).lower().strip(), str(year).lower().strip(), str(mileage).replace('km', '').lower().strip()]], columns=columns)
+            df2 = pd.DataFrame([["Kijiji", datePosted, str(title).lower().strip(), str(description).lower().strip(), url, str(transmission).lower().strip(), str(location).lower().strip(), str(price).lower().strip(), str(year).lower().strip(), str(mileage).replace('km', '').lower().strip()]], columns=columns)
             df = df.append(df2)
-
     return df
 
 def retrieveAutoTRADER(df):
@@ -89,41 +88,49 @@ def retrieveAutoTRADER(df):
     soup = bs(request.text, 'html.parser')
     count = 1
     for html in soup.find_all("div", "resultItem"):
-        print( "retrieving result ", count, end="\r")
-        count = count + 1
-        tag = html.find("span", "resultTitle")
-        title = tag.text
-        year = tag.contents[0].split()[0]
-        if not re.match(r"^[12][0-9]{3}$" , year):
-            year = None
+        try:
+            print( "retrieving result ", count, end="\r")
+            count = count + 1
+            tag = html.find("span", "resultTitle")
+            title = tag.text
+            year = tag.contents[0].split()[0]
+            if not re.match(r"^[12][0-9]{3}$" , year):
+                year = None
 
-        tag = html.find("h2").find("a")
-        url = str(tag['href'])
+            tag = html.find("h2").find("a")
+            url = str(tag['href'])
 
-        tag = html.find("p", "at_details")
-        description = tag.text
+            tag = html.find("p", "at_details")
+            description = tag.text
 
-        tag = html.find("div", "at_price")
-        price = tag.contents[-1]
+            tag = html.find("div", "at_price")
+            price = tag.contents[-1]
 
-        tag = html.find("div", "at_km")
-        mileage = tag.text
+            tag = html.find("div", "at_km")
+            mileage = tag.text
 
-        tag = html.find("div", "ResultDistance")
-        location = tag.contents[0]
+            tag = html.find("div", "ResultDistance")
+            location = tag.contents[0]
 
-        df2 = pd.DataFrame([["autoTRADER", None, title.lower().strip(), description.lower().strip(), url, None, location.lower().strip(), price.lower().strip(), year.lower().strip(), mileage.replace('km', '').lower().strip()]], columns=columns)
-        df = df.append(df2)
+            df2 = pd.DataFrame([["autoTRADER", None, title.lower().strip(), description.lower().strip(), url, None, location.lower().strip(), price.lower().strip(), year.lower().strip(), mileage.replace('km', '').lower().strip()]], columns=columns)
+            df = df.append(df2)
+        except Exception as e:
+            print("result error")
     return df
 
 def main():
     df = pd.DataFrame(columns=columns)
-    df = df.append(retrieveKijiji(df))
-    df = df.append(retrieveAutoTRADER(df))
+    df = retrieveKijiji(df)
+    df = retrieveAutoTRADER(df)
     numberResults = len(df.index)
     print("\ndone! " + str(numberResults) + " results found")
-    df = df.drop_duplicates()
+    df = df.drop_duplicates(["title", "year", "mileage", "price"])
+    df['YearScore'] = '=([@year] - MIN([year])) / (MAX([year]) - MIN([year]))'
+    df['MileageScore'] = '=(MAX([mileage]) - [@mileage]) / (MAX([mileage]) - MIN([mileage]))'
+    df['PriceScore'] = '=(MAX([price]) - [@price]) / (MAX([price]) - MIN([price]))'
+    df['TotalScore'] = '=[@MileageScore]+[@YearScore]+[@PriceScore]'
+    df['PMScore'] = '=[@MileageScore]+[@PriceScore]'
     df.to_csv(datetime.datetime.today().strftime("%Y-%m-%d") + "-CarSearchOutput.csv", index=False)
-
+    print("file saved")
 if __name__ == '__main__':
     main()
